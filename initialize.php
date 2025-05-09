5ace1ffb5a1a40811720842196642241392c413e
Miguelkarma <toro.paulmiguel.santos@gmail.com>
2025-05-10 00:01:22 +0800

    set up for hosting

--- initialize.php
+++ initialize.php
@@ -1,10 +1,10 @@
 <?php
 $dev_data = array('id'=>'-1','firstname'=>'Developer','lastname'=>'','username'=>'cicvet','password'=>'5da283a2d990e8d8512cf967df5bc0d0','last_login'=>'','date_updated'=>'','date_added'=>'');
-if(!defined('base_url')) define('base_url','http://localhost/ovas/');
+if(!defined('base_url')) define('base_url','vet-care.miguelkarma.rf.gd');
 if(!defined('base_app')) define('base_app', str_replace('\\','/',__DIR__).'/' );
 if(!defined('dev_data')) define('dev_data',$dev_data);
-if(!defined('DB_SERVER')) define('DB_SERVER',"localhost");
-if(!defined('DB_USERNAME')) define('DB_USERNAME',"root");
-if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"");
-if(!defined('DB_NAME')) define('DB_NAME',"ovas_db");
+if(!defined('DB_SERVER')) define('DB_SERVER',"sql113.infinityfree.com");
+if(!defined('DB_USERNAME')) define('DB_USERNAME',"if0_38923625");
+if(!defined('DB_PASSWORD')) define('DB_PASSWORD',"pFUbb6BB10");
+if(!defined('DB_NAME')) define('DB_NAME',"if0_38923625_ovas_db");
 ?>
